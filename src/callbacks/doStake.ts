58d6daf59911b9c5504f3dcde000dd040076f8fe
Ruslan Makarov <iwantletitdie@mail.ru>
2020-06-21 23:13:37 +0400

    feat: add doStake

--- src/callbacks/doStake.ts
+++ src/callbacks/doStake.ts
@@ -2,5 +2,15 @@ import { log } from '../logger';
 import { getCoefFromCoupon, getSumFromStake } from '../stake_functions';
 
 export function doStake(): boolean {
-  return true;
+  const stakeButton = document.querySelector(
+    '#slipBtnPlaceBet'
+  ) as HTMLDivElement;
+
+  if (stakeButton) {
+    stakeButton.click();
+    log('Нажимаем на кнопку "Сделать ставку"');
+    return true;
+  }
+  log('Кнопка "Сделать ставку" - не найдена');
+  return false;
 }
